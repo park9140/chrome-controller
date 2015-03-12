e209bcfdc688982f74092a873876fe45a6443810
boskya <bosky.atlani@changepoint.com>
2015-03-12 13:41:54 -0700

    up left right computation

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -5,13 +5,16 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendMessage) {
   console.log(message);
   switch (message) {
     case 'up':
+        goUp();
         break;
     case 'down':
         goDown();
         break;
     case 'left':
+        goLeft();
         break;
     case 'right':
+        goRight();
         break;
   }
 });
--- app/scripts/focusableElements.js
+++ app/scripts/focusableElements.js
@@ -16,6 +16,38 @@ function buildFocusablesMap() {
   return focusableMap;
 }
 
+function highlightElement(element) {
+  if (element) {
+    element.focus();
+    element.style.borderWidth = "5px";
+    element.style.borderStyle = "solid";
+    element.style.borderColor = "red";
+  }
+}
+
+function goUp() {
+  console.log('goUp called');
+  var focusableMap = buildFocusablesMap();
+  var currentFocusElement = document.activeElement;
+  var currentFocusElementRect = currentFocusElement.getBoundingClientRect();
+
+  var foundElement = null;
+  var prevDistance = 999999999;
+  var closestElement = null;
+  console.log(currentFocusElementRect);
+  focusableMap.forEach(function(elementRectangle, element, map){
+    if(elementRectangle.bottom < currentFocusElementRect.top) {
+      var currentDistance = elementDistance.getDistanceBetweenElements(currentFocusElementRect, elementRectangle);
+      console.log(currentDistance);
+      if(currentDistance < prevDistance) {
+        closestElement = element;
+        prevDistance = currentDistance;
+      }
+    }
+  });
+  console.log(closestElement);
+  highlightElement(closestElement);
+}
 
 function goDown() {
   console.log('goDown called');
@@ -38,12 +70,56 @@ function goDown() {
     }
   });
   console.log(closestElement);
-  closestElement.focus();
-  closestElement.style.borderWidth = "5px";
-  closestElement.style.borderStyle = "solid";
-  closestElement.style.borderColor = "red";
+  highlightElement(closestElement);
 }
 
+function goRight() {
+  console.log('goRight called');
+  var focusableMap = buildFocusablesMap();
+  var currentFocusElement = document.activeElement;
+  var currentFocusElementRect = currentFocusElement.getBoundingClientRect();
+
+  var foundElement = null;
+  var prevDistance = 999999999;
+  var closestElement = null;
+  console.log(currentFocusElementRect);
+  focusableMap.forEach(function(elementRectangle, element, map){
+    if(elementRectangle.left > currentFocusElementRect.right) {
+      var currentDistance = elementDistance.getDistanceBetweenElements(elementRectangle, currentFocusElementRect);
+      console.log(currentDistance);
+      if(currentDistance < prevDistance) {
+        closestElement = element;
+        prevDistance = currentDistance;
+      }
+    }
+  });
+  console.log(closestElement);
+  highlightElement(closestElement);
+}
+
+function goLeft() {
+  console.log('goLeft called');
+  var focusableMap = buildFocusablesMap();
+  var currentFocusElement = document.activeElement;
+  var currentFocusElementRect = currentFocusElement.getBoundingClientRect();
+
+  var foundElement = null;
+  var prevDistance = 999999999;
+  var closestElement = null;
+  console.log(currentFocusElementRect);
+  focusableMap.forEach(function(elementRectangle, element, map){
+    if(elementRectangle.right < currentFocusElementRect.left) {
+      var currentDistance = elementDistance.getDistanceBetweenElements(elementRectangle, currentFocusElementRect);
+      console.log(currentDistance);
+      if(currentDistance < prevDistance) {
+        closestElement = element;
+        prevDistance = currentDistance;
+      }
+    }
+  });
+  console.log(closestElement);
+  highlightElement(closestElement);
+}
 
 window.addEventListener('load', function() {
 });
