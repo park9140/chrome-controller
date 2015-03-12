892c08040830e25579523f04a606a53cd7eee712
Adam Kalman <akalman@daptiv.com>
2015-03-12 10:50:13 -0700

    buttons fire an event
    in addition to a console debug statement, pressing a button now fires an event to eventually be picked up and translated into chrome controller action events.  Also the non-working deregistration of the setInterval has been removed, it should probably be added in later.  I mean, unless we want to crash our users browsers.  Personally, I'm indifferent.

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -9,19 +9,20 @@ function getPollFn(gamepadIndex) {
     for (var i = 0; i < gamepad.buttons.length; i++) {
       if (gamepad.buttons[i].pressed && lastGamepad.buttons[i] !== gamepad.buttons[i].value) {
         console.log('controller button ' + i + ' pressed with value ' + gamepad.buttons[i].value);
+
+        var event = new CustomEvent('controller.buttonPressed', { detail: { controllerIndex: gamepadIndex, buttonIndex: i, value: gamepad.buttons[i].value } });
+        window.dispatchEvent(event);
       }
       lastGamepad.buttons[i]= gamepad.buttons[i].value;
     }
   }
 }
 
-var thinkers = [];
-
-window.addEventListener("gamepadconnected", function(e) {
-  console.log('gamepad connected');
-  thinkers[e.gamepad.index] = setInterval(getPollFn(e.gamepad.index), 100);
+window.addEventListener('controller.buttonPressed', function(e) {
+  console.log('button event handled: ', e.detail);
 });
 
-window.addEventListener("gamepaddisconnected", function(e) {
-  thinkers[e.gamepad.index]();
+window.addEventListener('gamepadconnected', function(e) {
+  console.log('gamepad connected');
+  setInterval(getPollFn(e.gamepad.index), 100);
 });
