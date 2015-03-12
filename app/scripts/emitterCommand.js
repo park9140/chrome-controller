ce2ab26ebaa977a73f248436712c30b870d173d5
Neil Breeden III <neil.breeden@changepoint.com>
2015-03-12 12:46:52 -0700

    Adding getParam function to emitterCommand object.

--- app/scripts/emitterCommand.js
+++ app/scripts/emitterCommand.js
@@ -1,5 +1,12 @@
 function emitterCommand(commandName, commandParamArray) {
     this.commandName = commandName;
+
+    if (commandParamArray) {
+      for (var i=0; i<this.commandParamArray.length; i++) {
+        commandParamArray[i].name = commandParamArray[i].toLowerCase();
+      }
+    }
+
     this.commandParameters = commandParamArray ? commandParamArray : [];
 }
 
@@ -9,7 +16,23 @@ function emitterCommandParam(name, value) {
 }
 
 emitterCommand.prototype.addParam = function(key, value) {
-  this.commandParameters.push(new emitterCommandParam(key, value));
+  this.commandParameters.push(new emitterCommandParam(key.toLowerCase(), value));
   return this;
+}
+
+emitterCommand.prototype.getParam = function(key) {
+  if (!key) {
+    return null;
+  }
+
+  key = key.toLowerCase();
+
+  for (var i=0; i<this.commandParameters.length; i++) {
+    if (this.commandParameters[i].name === key) {
+      return this.commandParameters[i].value;
+    }
+  }
+
+  return null;
 }
 
