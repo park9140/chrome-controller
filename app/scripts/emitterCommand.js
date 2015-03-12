e3da3adf8c1538bd9cb97ea26db717bca3263059
Neil Breeden III <neil.breeden@changepoint.com>
2015-03-12 14:15:40 -0700

    Moved addParam and getParam functions to prototype object.

--- app/scripts/emitterCommand.js
+++ app/scripts/emitterCommand.js
@@ -6,29 +6,29 @@ function emitterCommand(commandName, commandParamArray) {
         commandParamArray[i].name = commandParamArray[i].name.toLowerCase();
       }
     }
-    
-    this.addParam = function(key, value) {
-      this.commandParameters.push(new emitterCommandParam(key.toLowerCase(), value));
-      return this;
-    }
-
-    this.getParam = function(key) {
-      if (!key) {
-        return null;
-      }
-
-      key = key.toLowerCase();
-
-      for (var i=0; i<this.commandParameters.length; i++) {
-        if (this.commandParameters[i].name === key) {
-          return this.commandParameters[i].value;
-        }
-      }
-
-      return null;
-    }
 
     this.commandParameters = commandParamArray ? commandParamArray : [];
+}
+
+emitterCommand.prototype.addParam = function(key, value) {
+  this.commandParameters.push(new emitterCommandParam(key.toLowerCase(), value));
+  return this;
+}
+
+emitterCommand.prototype.getParam = function(key) {
+  if (!key) {
+    return null;
+  }
+
+  key = key.toLowerCase();
+
+  for (var i=0; i<this.commandParameters.length; i++) {
+    if (this.commandParameters[i].name === key) {
+      return this.commandParameters[i].value;
+    }
+  }
+
+  return null;
 }
 
 function emitterCommandParam(name, value) {
