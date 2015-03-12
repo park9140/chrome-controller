3093f08ca10089d6790e1d95f9468c07218f8f3e
Adam Kalman <akalman@daptiv.com>
2015-03-12 10:20:58 -0700

    script to load and detect button presses
    not sure if it will work within the plugin, but it successfully detects a button being pressed (and ignores buttons being held)

---
+++ app/scripts/controller.js
@@ -0,0 +1,27 @@
+function getPollFn(gamepadIndex) {
+  var lastGamepad = {
+    buttons: []
+  };
+
+  return function() {
+    var gamepad = navigator.getGamepads()[gamepadIndex];
+
+    for (var i = 0; i < gamepad.buttons.length; i++) {
+      if (gamepad.buttons[i].pressed && lastGamepad.buttons[i] !== gamepad.buttons[i].value) {
+        console.log('controller button ' + i + ' pressed with value ' + gamepad.buttons[i].value);
+      }
+      lastGamepad.buttons[i]= gamepad.buttons[i].value;
+    }
+  }
+}
+
+var thinkers = [];
+
+window.addEventListener("gamepadconnected", function(e) {
+  console.log('gamepad connected');
+  thinkers[e.gamepad.index] = setInterval(getPollFn(e.gamepad.index), 100);
+});
+
+window.addEventListener("gamepaddisconnected", function(e) {
+  thinkers[e.gamepad.index]();
+});
